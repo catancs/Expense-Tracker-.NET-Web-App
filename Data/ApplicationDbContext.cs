d34def014807eb1c325e1e9443251a845f5945be
catancs <catalinniculescu20@gmail.com>
2024-04-28 21:02:11 +0200

    update ApplicationDB

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using ToDo.Models; // Replace 'to_do' with the actual namespace of your application
+using ToDo.Models; 
 
 public class ApplicationDbContext : DbContext
 {
@@ -12,18 +12,15 @@ public class ApplicationDbContext : DbContext
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<Income> Incomes { get; set; }
 
-    // Add any additional configuration here
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        // For example, to configure a one-to-many relationship between Category and Expense
         modelBuilder.Entity<Category>()
             .HasMany(c => c.Expenses)
             .WithOne(e => e.Category)
             .HasForeignKey(e => e.CategoryId);
 
-        // Similar configuration for Income if necessary
         modelBuilder.Entity<Category>()
             .HasMany(c => c.Incomes)
             .WithOne(i => i.Category)
